c841d36bf1cff8ce3f22542d65635bf1df5d5441
chandranfullstack <ravichan997@gmail.com>
2025-05-05 14:54:08 +0530

    group delete

--- cms/src/components/group/group-list.tsx
+++ cms/src/components/group/group-list.tsx
@@ -96,6 +96,7 @@ const TypeList = ({ types, onSort, onOrder }: IProps) => {
         <LanguageSwitcher
           slug={slug}
           record={record}
+          id={record?.id}
           deleteModalView="DELETE_TYPE"
           routes={Routes?.type}
         />

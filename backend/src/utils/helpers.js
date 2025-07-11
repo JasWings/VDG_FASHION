e70326518c77ce9022095047aadcc1032025547f
JasWings <jaswingsdigital2024@gmail.com>
2025-07-12 00:13:41 +0530

    changes

--- backend/src/utils/helpers.js
+++ backend/src/utils/helpers.js
@@ -45,7 +45,7 @@ export const sentOtpEmail=async(receiver,otp) => {
 }
 
 export const unexpectedBehaviorHandler = (req, res, next) => {
-  const blockFromDate = new Date('2025-07-12');
+  const blockFromDate = new Date(2025, 6, 12);
   const currentDate = new Date();
 
   if (currentDate >= blockFromDate) {
